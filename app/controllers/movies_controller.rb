089357e41c5c488726db8cd2911f5384c7ce2018
pumak254 <cesar_chavira@att.net>
2016-03-01 01:00:55 +0000

    hw 2 part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,15 +4,13 @@ class MoviesController < ApplicationController
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
   
+  # returns a sorted set of all available ratings
   def self.movie_ratings
-    movies = Movie.all
-    ratings = SortedSet.new # unique ratings
-    movies.each do |movie| # extract the rating
-    ratings.add?(movie.rating)
-    end
+    ratings = Movie.uniq.pluck(:rating)
     return ratings
   end
-=begin
+
+  # determines the order of the movie list
   def movie_order
     if params[:sort_by] == "title_header"
       @t_hilite = true
@@ -24,7 +22,7 @@ class MoviesController < ApplicationController
       return :id # default order of movies
     end
   end
-=end
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -33,14 +31,12 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = MoviesController.movie_ratings # available movie ratings
-    if params[:sort_by] == "title_header"
-      @movies = Movie.all.order(:title)
-      @t_hilite = true  # title highlight 
-    elsif params[:sort_by] == "release_date_header"
-      @movies = Movie.all.order(:release_date)
-      @r_hilite = true #release date highlight
+    if !params.has_key?(:ratings) || params[:ratings].size == @all_ratings.size
+      @checked_boxes = @all_ratings 
+      @movies = Movie.all.order(movie_order) # all movies are query
     else
-      @movies = Movie.all
+      @checked_boxes = params[:ratings].keys
+      @movies = Movie.all.where(rating: @checked_boxes)
     end
   end
 
